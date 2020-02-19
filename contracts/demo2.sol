c5864a1d392b12830d59f50c62b1607b9dd4975d
Achal Arya <47027130+dirarya@users.noreply.github.com>
2020-02-19 15:14:52 +0530

    hello

---
+++ contracts/demo2.sol
@@ -0,0 +1,20 @@
+//write your code ...
+
+
+pragma solidity  ^0.4.23;
+
+contract StateVariablesAndFunctions {
+
+    //state variable
+    string statement;
+
+    //set statement function
+    function setStatement(string _statement) public  {
+        statement = _statement;
+    }
+
+    //get statement function
+    function getStatement() public view returns(string) {
+        return statement;
+    }
+}
